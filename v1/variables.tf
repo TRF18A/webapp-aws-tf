71366561c543e60017df53c2f1a413450c1fe267
TRF18A <39011287+TRF18A@users.noreply.github.com>
2018-05-06 12:20:56 +0800

    Update variables.tf

--- v1/variables.tf
+++ v1/variables.tf
@@ -1,7 +1,9 @@
 variable "access_key" {}
 variable "secret_key" {}
+variable "keypair" {}
 variable "ami"{}
 variable "instance_type" {}
+
 variable "az_1" {}
 variable "az_2" {}
 variable "vpc_cidr_block" {}
@@ -10,5 +12,6 @@ variable "subnet_web2_cidr_block" {}
 variable "subnet_db1_cidr_block" {}
 variable "subnet_db2_cidr_block" {}
 variable "region" {}
+
 variable "db_username" {}
 variable "db_password" {}
