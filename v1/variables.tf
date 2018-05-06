e6eae4b974cc0dceb9ee15cf97f2573978ba6983
TRF18A <39011287+TRF18A@users.noreply.github.com>
2018-05-06 11:18:22 +0800

    Create variables.tf

---
+++ v1/variables.tf
@@ -0,0 +1,14 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "ami"{}
+variable "instance_type" {}
+variable "az_1" {}
+variable "az_2" {}
+variable "vpc_cidr_block" {}
+variable "subnet_web1_cidr_block" {}
+variable "subnet_web2_cidr_block" {}
+variable "subnet_db1_cidr_block" {}
+variable "subnet_db2_cidr_block" {}
+variable "region" {}
+variable "db_username" {}
+variable "db_password" {}
